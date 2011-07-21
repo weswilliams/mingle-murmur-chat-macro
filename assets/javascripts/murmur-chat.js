95dbed3896ad621716bc6cbd2dd7ae09924e3dd2
Wes Williams <wes.williamz@gmail.com>
2011-07-21 13:24:48 -0500

    - added icon and fixed layout issues

--- assets/javascripts/murmur-chat.js
+++ assets/javascripts/murmur-chat.js
@@ -2,21 +2,32 @@ var murmurChat = {};
 
 murmurChat.init = function($, macroDebug) {
 
-  try {
+  var success = function(xml, textStatus, jqXHR) {
+    macroDebug.log('received status: ' + textStatus);
+    var murmur, murmurXML, iconPath;
+    $(xml).find('murmur').each(function() {
+      murmur = $('#murmur-template').clone();
+      murmurXML = $(this);
+      iconPath = murmurXML.find('icon_path').text();
+      if (!iconPath) {
+        iconPath = "../../../../plugin_assets/murmur_chat/images/default_user_avatar.png";
+      }
+      macroDebug.log("setting icon to: [" + iconPath + "]");
+      $('#user-image', murmur).attr('src',iconPath);
+      $('#message', murmur).text(murmurXML.find('body').text());
+      $('#user-name', murmur).text(murmurXML.find('name').text());
+      $('#murmur-messages').append(murmur);
+    });
+  };
 
-    var success = function(xml, textStatus, jqXHR) {
-      macroDebug.log('received status: ' + textStatus);
-      $(xml).find('body').each(function() {
-        $('#chat-messages').append($(this).text()).append('<br>');
-      });
-    };
+  try {
 
     $("#debug-info").ajaxError(function(event, jqXHR, ajaxSettings, thrownError) {
       $(this).append("Triggered ajaxError handler: " + thrownError);
     });
 
     macroDebug.log('making murmur request');
-    $.get('/api/v2/projects/baml_team_1/murmurs.xml?since_id=312', success);
+    $.get('/api/v2/projects/baml_team_1/murmurs.xml', success);
 
   } catch(err) {
     macroDebug.log(err);
