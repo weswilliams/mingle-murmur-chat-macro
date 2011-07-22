367ecc0d056a1cf754616a7c1a4ab46f3b70f205
Wes Williams <wes.williamz@gmail.com>
2011-07-22 15:40:01 -0500

    - hide debug log when debug disabled

--- assets/javascripts/debug.js
+++ assets/javascripts/debug.js
@@ -1,13 +1,14 @@
 murmurChat.macroDebugInit = function(enabled, out) {
-    var that = {};
-    that.log = function(msg) {
-        if (!enabled) {
-            return;
-        }
-        out.append(new Date() + ' - ' + msg + '<br/>');
-    };
-    that.log('debug log starting');
-    murmurChat.log = that.log;
-    return that;
+  var that = {};
+  that.log = function(msg) {
+    if (!enabled) {
+      out.hide();
+      return;
+    }
+    out.append(new Date() + ' - ' + msg + '<br/>');
+  };
+  that.log('debug log starting');
+  murmurChat.log = that.log;
+  return that;
 };
 
